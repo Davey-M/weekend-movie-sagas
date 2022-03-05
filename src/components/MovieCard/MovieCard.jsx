17252d27d9316c02a4f65faefb73bfbe2c1ca002
Davey-M <davey.meuer@gmail.com>
2022-03-05 13:07:50 -0600

    add styling to MovieCard component

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,8 +1,15 @@
+import { useHistory } from 'react-router-dom';
 import './MovieCard.css';
 
-function MovieCard({ title, poster }) {
+function MovieCard({ title, poster, id }) {
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push(`/details/${id}`);
+    };
+
     return (
-        <div className='movie-card'>
+        <div className='movie-card' onClick={handleClick}>
             <img className='movie-card-img' src={poster} alt={title} />
         </div>
     );
