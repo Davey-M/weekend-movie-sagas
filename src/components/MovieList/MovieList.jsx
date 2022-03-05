0737ce8ce7aba679b9af0701cbb1ce9cd6301846
Davey-M <davey.meuer@gmail.com>
2022-03-05 12:40:48 -0600

    import MovieCard

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,10 +2,16 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css';
 
+// component imports
+import MovieCard from '../MovieCard/MovieCard';
+
 function MovieList() {
     const dispatch = useDispatch();
+
+    // store movies in a redux reducer
     const movies = useSelector((store) => store.movies);
 
+    // get movies on initial load
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
@@ -15,12 +21,8 @@ function MovieList() {
             <h1>MovieList</h1>
             <section className='movies'>
                 {movies.map((movie) => {
-                    return (
-                        <div key={movie.id}>
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title} />
-                        </div>
-                    );
+                    // MovieCard takes two props poster and title
+                    return <MovieCard key={movie.id} {...movie} />;
                 })}
             </section>
         </main>
